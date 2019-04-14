52a93b266622e17ae1957722b5a2949db3a7e3d2
viktorija <tosheva.victoria@yahoo.com>
2019-04-14 13:26:49 +0200

    first commit

---
+++ src/main/java/com/example/sitemap/SitemapApplication.java
@@ -0,0 +1,13 @@
+package com.example.sitemap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitemapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitemapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sitemap/SitemapApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.sitemap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SitemapApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
